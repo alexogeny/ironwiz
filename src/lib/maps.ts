762c7537fe8a6e1f12c622c76ed5fb5c392967df
alexogeny <6896115+alexogeny@users.noreply.github.com>
2023-09-20 22:13:18 +1000

    feat: map skill indices to strings

---
+++ src/lib/maps.ts
@@ -0,0 +1,16 @@
+// a map of the skill number strings e.g. "1" => "Woodcutting"
+export const skillMap = {
+  "1": "Woodcutting",
+  "2": "Mining",
+  "3": "Smelting",
+  "4": "Smithing",
+  "5": "Forging",
+  "6": "One-handed",
+  "7": "Two-handed",
+  "8": "Defence",
+  "9": "Fishing",
+  "10": "Cooking",
+  "12": "Alchemy",
+  "13": "Farming",
+  "14": "Ranged",
+};
